5a9b9d7547cbed69bbbf1ec573d10bdb757fddd5
GHSoyad <ghsoyad@gmail.com>
2022-12-25 20:10:57 +0600

    Added news letter section

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Footer/Footer'
-import NewsLetter from '../components/NewsLetter/NewsLetter'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +10,6 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <NewsLetter></NewsLetter>
       <Footer />
     </div>
   )
