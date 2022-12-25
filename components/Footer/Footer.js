fcfcb9b10cb1f6b2cede71237a793f29e14be192
GHSoyad <ghsoyad@gmail.com>
2022-12-25 20:08:33 +0600

    Added news letter section

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer aria-label="Site Footer" className="bg-white">
-            <div className="mx-auto max-w-screen-xl px-4 pt-16 pb-6 sm:px-6 lg:px-8 lg:pt-24">
+            <div className="mx-auto px-4 pt-16 pb-6 sm:px-6 lg:px-8 lg:pt-24">
                 <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
                     <div>
                         <div className="flex justify-center text-teal-600 sm:justify-start">
