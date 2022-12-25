6972c16d620a62d8b8d11d43273e26db5631bdf0
almahfuz <almahfuz0327@gmail.com>
2022-12-25 18:42:45 +0600

    attached footer to home

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   )
 }
