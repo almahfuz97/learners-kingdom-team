17fcac5ea893e4e04d7e7df6672cd2a0121f4af9
almahfuz <almahfuz0327@gmail.com>
2022-12-25 19:23:09 +0600

    added search box and icon on banner

---
+++ components/Banner/Banner.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Banner() {
+    return (
+        <div className='mt-16 flex justify-center'>
+            <div className=' flex'>
+                <input placeholder=' search books' type="text" className='border border-primary_color focus:border-r-0 focus:border-secondary_color rounded-lg rounded-r-none p-4' />
+                <Image src={'/search-white.png'} width={56} height={56} className=' bg-primary_color rounded-r cursor-pointer hover:shadow'></Image>
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Footer/Footer'
+import Banner from '../components/Banner/Banner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,6 +11,7 @@ export default function Home() {
   return (
     <div>
       <Navbar />
+      <Banner />
       <Footer />
     </div>
   )
