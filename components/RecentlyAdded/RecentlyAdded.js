ad92f8c402bb1c05d36b8abe8c218513206e2c7f
unknown <parvezmrf@gmail.com>
2022-12-26 03:54:04 +0600

    new book realease style add arrow

--- components/RecentlyAdded/RecentlyAdded.js
+++ components/RecentlyAdded/RecentlyAdded.js
@@ -2,14 +2,25 @@ import React, { Component } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+
 import RecentlyAddedCard from "./RecentlyAddedCard";
 
 export default class Responsive extends Component {
 
+    constructor(props) {
+        super(props);
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+    }
+    next() {
+        this.slider.slickNext();
+    }
+    previous() {
+        this.slider.slickPrev();
+    }
+
+
     render() {
-
-
-
 
         const settings = {
             dots: false,
@@ -53,13 +64,26 @@ export default class Responsive extends Component {
 
                 <h2 className="text-2xl font-bold my-8" > New Books Releases </h2>
 
+                <div >
 
-                <Slider {...settings}>
+                    <button className="button mx-2 text-2xl p-2 rounded-full bg-secondary_color" onClick={this.previous}>
+
+                        <img src="/left-arrow.svg" alt="book" class="w-8" />
+                    </button>
+                    <button className="button mx-2 text-2xl p-2 rounded-full bg-secondary_color" onClick={this.next}>
+                        <img src="/right-arrow.svg" alt="book" class="w-8" />
+
+                    </button>
+                </div>
+
+
+                <Slider ref={c => (this.slider = c)} {...settings}>
                     {
 
                         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map(card => <RecentlyAddedCard></RecentlyAddedCard>)
                     }
                 </Slider>
+
             </div>
 
         );
