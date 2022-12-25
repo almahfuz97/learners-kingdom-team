6e22f29a8f15d211bf9bd0951c5d38c570d940c4
almahfuz <almahfuz0327@gmail.com>
2022-12-25 19:26:36 +0600

    added animation to search icon

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -6,7 +6,9 @@ export default function Banner() {
         <div className='mt-16 flex justify-center'>
             <div className=' flex'>
                 <input placeholder=' search books' type="text" className='border border-primary_color focus:border-r-0 focus:border-secondary_color rounded-lg rounded-r-none p-4' />
-                <Image src={'/search-white.png'} width={56} height={56} className=' bg-primary_color rounded-r cursor-pointer hover:shadow'></Image>
+                <div className=' bg-primary_color rounded-r cursor-pointer hover:shadow'>
+                    <Image src={'/search-white.png'} width={56} height={56} className=' animate-pulse' ></Image>
+                </div>
             </div>
         </div>
     )
