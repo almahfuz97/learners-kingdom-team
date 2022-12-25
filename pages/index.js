3439f8bb1dd396e2fc9056013131f8a8d8a60b2a
almahfuz <almahfuz0327@gmail.com>
2022-12-25 15:49:35 +0600

    deleted default global

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='mt-4'>
-      <h3 className=' text-2xl text-center'>Hello Coders!</h3>
-      <h1 className=' text-4xl text-center'>Welcome to your first Team Project</h1>
+    <div>
+
     </div>
   )
 }
