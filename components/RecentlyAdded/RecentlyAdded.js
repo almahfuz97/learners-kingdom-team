e56367e690254107d13ea0572e1faded6e948389
unknown <parvezmrf@gmail.com>
2022-12-25 21:38:05 +0600

    new realease modify

--- components/RecentlyAdded/RecentlyAdded.js
+++ components/RecentlyAdded/RecentlyAdded.js
@@ -4,9 +4,8 @@ const RecentlyAdded = () => {
     return (
         <div className="my-6">
 
-            <h2 className="text-2xl">   New Book Added</h2>
+            <h2 className="text-2xl">New Book Releases</h2>
             <div className=" flex flex-wrap gap-4">
-                <RecentlyAddedCard />
                 <RecentlyAddedCard />
                 <RecentlyAddedCard />
                 <RecentlyAddedCard />
