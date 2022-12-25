507c3b74f16a98e6c063a43ed972c2fce7d92a74
ImtiazUddin28 <imtiaz100463@gmail.com>
2022-12-25 20:00:11 +0600

    pricing section added

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Footer/Footer'
+import Pricing from '../components/Pricing/Pricing'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,6 +11,7 @@ export default function Home() {
   return (
     <div>
       <Navbar />
+      <Pricing/>
       <Footer />
     </div>
   )
