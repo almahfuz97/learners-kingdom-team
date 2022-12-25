b55c4b32c15ca24a9a6a65a9b7821616ae2dd2ed
unknown <parvezmrf@gmail.com>
2022-12-26 03:00:12 +0600

    new book realease styly added done

--- components/RecentlyAdded/RecentlyAdded.js
+++ components/RecentlyAdded/RecentlyAdded.js
@@ -14,18 +14,17 @@ export default class Responsive extends Component {
         const settings = {
             dots: false,
             infinite: true,
-            centerMode: true,
             speed: 1000,
             slidesToShow: 4,
             slidesToScroll: 4,
             autoplay: true,
-            initialSlide: 0,
+            initialSlide: 4,
             responsive: [
                 {
                     breakpoint: 1024,
                     settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2,
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
                         infinite: true,
                         dots: true
                     }
