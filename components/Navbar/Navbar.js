f00269384c141d1f27fb7858968527b4c24ae1de
ImtiazUddin28 <imtiaz100463@gmail.com>
2022-12-28 14:01:00 +0600

    routes path added in navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar() {
                     <ul className='hidden md:flex gap-4'>
                         <Link className=' hover:text-primary_color' href="/"><li>Home</li></Link>
                         <Link className=' hover:text-primary_color' href="/about"><li>About Us</li></Link>
-                        <Link className=' hover:text-primary_color' href="/"><li>Sign Up</li></Link>
+                        <Link className=' hover:text-primary_color' href="/register"><li>Sign Up</li></Link>
                     </ul>
                     <div className='block md:hidden hover:cursor-pointer' onClick={handleToggle} >
                         <div className={` h-1 w-6 mb-1  bg-primary_color ${toggle ? ' origin-right rotate-45 translate-x-2 translate-y-2' : 'rotate-0'}`}></div>
@@ -29,8 +29,8 @@ export default function Navbar() {
                     <div className={`absolute bg-secondary_color md:hidden mt-3 z-50 p-8 uppercase font-bold text-neutral ${toggle ? ' right-0 top-20  h-screen  duration-500' : 'duration-500 -top-96  right-0'}`}>
                         <ul className=' flex flex-col gap-4'>
                             <Link className=' hover:text-primary_color' href="#"><li>Home</li></Link>
-                            <Link className=' hover:text-primary_color' href="#"><li>About Us</li></Link>
-                            <Link className=' hover:text-primary_color' href="#"><li>Sign Up</li></Link>
+                            <Link className=' hover:text-primary_color' href="/about"><li>About Us</li></Link>
+                            <Link className=' hover:text-primary_color' href="/register"><li>Sign Up</li></Link>
                         </ul>
                     </div>
                 </div>
