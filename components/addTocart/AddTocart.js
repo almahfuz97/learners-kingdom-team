2db6ad383b36a755394d92312df1060b840145ac
unknown <parvezmrf@gmail.com>
2022-12-28 18:42:41 +0600

    dynamic category. add to cart design

--- components/addTocart/AddTocart.js
+++ components/addTocart/AddTocart.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import AddTocartCard from "./AddTocartCard";
 
 const AddTocart = () => {
+    // const [addToCart, SetAddToCrt] = useState([])
+
+    // useEffect(() => {
+    //     fetch('')
+    //         .then(res => res.json())
+    //         .then(data => SetAddToCrt(data))
+    // }, [])
+
+
+
     return (
         <div>
+
             <AddTocartCard></AddTocartCard>
 
         </div>
