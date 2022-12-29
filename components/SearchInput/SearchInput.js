0880eb1926959b41e00fb09802de0b65a4c04e04
GHSoyad <ghsoyad@gmail.com>
2022-12-30 00:29:53 +0600

    Updated user auth

--- components/SearchInput/SearchInput.js
+++ components/SearchInput/SearchInput.js
@@ -28,7 +28,7 @@ export default function SearchInput({ register, watch, searchedBook, handleKeyUp
                 </style>
                 <div className='relative'>
                     <input onKeyUp={handleKeyUp}  {...register('search')} placeholder='Search books' type="text" className={`border border-primary_color focus:border-r-0 focus:border-secondary_color rounded-lg ${watch('search')?.length > 0 && 'rounded-bl-none'} rounded-r-none p-2 lg:p-4 lg:min-w-[400px] max-h-[40px] lg:max-h-[55px] outline-none`} />
-                    <div className='absolute  font-bold bg-secondary_color rounded-b-lg w-full max-h-[400px] lg:max-h-[300px] overflow-scroll '>
+                    <div className='absolute font-bold bg-secondary_color rounded-b-lg w-full max-h-[400px] lg:max-h-[300px] overflow-y-scroll'>
                         {
                             searchedBook?.map(book => <SearchCard key={book._id} book={book} />)
                         }
