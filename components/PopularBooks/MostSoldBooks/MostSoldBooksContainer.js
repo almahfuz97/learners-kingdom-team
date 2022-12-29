7865e901f425fae7b7cb495b94638e4b64345afb
almahfuz <almahfuz0327@gmail.com>
2022-12-29 06:22:22 +0600

    grid cols 3 to most sold

--- components/PopularBooks/MostSoldBooks/MostSoldBooksContainer.js
+++ components/PopularBooks/MostSoldBooks/MostSoldBooksContainer.js
@@ -6,7 +6,7 @@ const MostSoldBooksContainer = ({ mostSoldBooks }) => {
 			<h1 className="text-3xl mb-8 text-primary_color font-bold text-center my-2">
 				Most Sold Books
 			</h1>
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 				{mostSoldBooks?.map((book, index) => (
 					<MostSoldBookCard book={book} key={index} />
 				))}
