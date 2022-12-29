3b6ccd7dd9fde10fb8d4094d4fb4288dbcf64076
ImtiazUddin28 <imtiaz100463@gmail.com>
2022-12-29 22:16:57 +0600

    user profile ready

---
+++ pages/profile.js
@@ -0,0 +1,12 @@
+import UserProfile from "../components/UserProfile/UserProfile";
+
+
+const profile = () => {
+    return (
+        <div>
+            <UserProfile></UserProfile>
+        </div>
+    );
+};
+
+export default profile;
