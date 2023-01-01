7a8815a5abbd6455e0cf8ea2584c7acccd26d81c
almahfuz <almahfuz0327@gmail.com>
2023-01-01 15:22:31 +0600

    changed next.config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
-        port: '',
-        pathname: '/**/**',
-      },
-    ],
-  },
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
 module.exports = {
-  nextConfig,
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
@@ -28,8 +18,6 @@ module.exports = {
   env: {
     URL: 'http://localhost:3000',
   },
-
-
 };
 
 // module.exports = {
