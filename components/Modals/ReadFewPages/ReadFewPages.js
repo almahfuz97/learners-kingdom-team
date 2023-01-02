c359073e65c3ae992c5763ac2d2b760fdc9f0d87
almahfuz <almahfuz0327@gmail.com>
2023-01-02 20:23:06 +0600

    added profile dash

--- components/Modals/ReadFewPages/ReadFewPages.js
+++ components/Modals/ReadFewPages/ReadFewPages.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ReadFewPages({ setModalToggle, book }) {
     return (
-        <div id='modal' className="relative mx-auto md:w-1/2 w-4/5 max-h-[400px] overflow-auto mt-20 opacity-100 bg-secondary_color text-white  p-6 rounded-md  sm:py-8 sm:px-12 shadow-lg">
+        <div id='modal' className="relative mx-auto md:w-1/2 w-4/5 max-h-[400px] overflow-auto mt-20 opacity-100 bg-slate-50 text-white  p-6 rounded-md  sm:py-8 sm:px-12 shadow-lg">
             <button onClick={() => setModalToggle(prev => !prev)} className="absolute top-2 right-2">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="flex-shrink-0 w-6 h-6">
                     <polygon points="427.314 107.313 404.686 84.687 256 233.373 107.314 84.687 84.686 107.313 233.373 256 84.686 404.687 107.314 427.313 256 278.627 404.686 427.313 427.314 404.687 278.627 256 427.314 107.313"></polygon>
--- pages/dashboard/purchased.js
+++ pages/dashboard/purchased.js
@@ -19,7 +19,7 @@ export default function Purchased() {
     }, [user?.email])
 
     return (
-        <div className='min-h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             {
                 books?.map(book => <OrderedBookCard book={book} />)
             }
